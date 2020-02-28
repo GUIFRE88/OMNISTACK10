4a17e44e1b3071ff2584b247385cd65d9f7a9a9e
Guilherme Freudenburg <guilherme.freudenburg@ng.local>
2020-02-28 15:52:18 -0300

    Adicionado botão para pesquisa em tela.

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, Image, View, Text } from 'react-native'
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native'
 import MapView, { Marker, Callout } from 'react-native-maps'
 
-import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { requestPermissionsAsync, getCurrentPositionAsync, stopLocationUpdatesAsync } from 'expo-location'
+
+import { MaterialIcons } from '@expo/vector-icons'
 
 function Main({navigation}){
 
@@ -35,7 +37,9 @@ function Main({navigation}){
         return null
     }
 
-    return <MapView  initialRegion={currentRegion} style={styles.map}>
+    return (
+        <>
+        <MapView  initialRegion={currentRegion} style={styles.map}>
         <Marker coordinate={{ latitude: -27.2111164, longitude: -49.6374491}}>
             <Image style={styles.avatar} source={ { uri: 'https://avatars1.githubusercontent.com/u/36928790?s=460&v=4'} }/>
             <Callout onPress={ ()=>{ // Cria função de navegação ao ter o click
@@ -48,7 +52,20 @@ function Main({navigation}){
                 </View>
             </Callout>
         </Marker>
-    </MapView>
+        </MapView>
+        <View style={styles.searchForm}>
+            <TextInput  style={styles.searchInput}
+                        placeholder="Buscar devs por Techs..."
+                        placeholderTextColor="#999"
+                        autoCapitalize="words"
+                        autoCorrect={false}
+            />
+            <TouchableOpacity onPress={ () => {} } style={styles.loadButton}>
+                <MaterialIcons name="my-location"  size={20} color="#FFF"/>
+            </TouchableOpacity>
+        </View>
+    </>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -78,6 +95,39 @@ const styles = StyleSheet.create({
     },
     devTechs:{
         marginTop: 5,
+    },
+    searchForm:{
+        position: 'absolute',
+        bottom: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+    searchInput:{
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2, 
+    },
+    loadButton:{
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4Dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
     },
 })
 
